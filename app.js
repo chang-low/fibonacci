dd6d25a301eebe21ad47b9700fab2aa5a302fd9f
chang-low <shige19910508@gmail.com>
2020-04-19 22:31:14 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 function fib(n) {
     if (n === 0) {
 	return 0;    
@@ -13,3 +14,22 @@ for (let itr = 0; itr <= length; itr++) {
     console.log(fib(itr));
 }
 
+*/
+
+const memo = new Map();
+memo.set(0,0);
+memo.set(1,1);
+
+function fib(n) {
+    if(memo.has(n)) {
+        return memo.get(n);
+    }
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
+}
+
+const length = 10;
+for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+}
